d8770fa9d026a49b244b577e83d18d3bdf14a52b
Guillaume Morel <guillaume.morel763@gmail.com>
2020-08-20 14:32:48 +0200

    add boostrap 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,104 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { GoogleLogin } from 'react-google-login';
 
-const responseGoogle = (response: any) => {
-  console.log(response);
+import {GoogleLogin, GoogleLogout} from 'react-google-login';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+} from "react-router-dom";
+
+import {Nav, Navbar, NavDropdown, Container} from "react-bootstrap";
+
+
+interface AppState {
+    isSignedIn: boolean
+    name: string
 }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <GoogleLogin
+export default class App extends React.Component<any, AppState> {
+
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            isSignedIn: false,
+            name: "Anonymous"
+        }
+    }
+
+    responseGoogle = (response: any) => {
+        console.log(response)
+        this.setState(
+            {
+                isSignedIn: true,
+                name: response.profileObj.name
+            }
+        )
+    }
+
+    render = () => (
+        <Router>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Link to="/">
+                    <Navbar.Brand href="/">Game Center</Navbar.Brand></Link>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <NavDropdown title="Games" id="collasible-nav-dropdown">
+                            <Link to="/battleship"><NavDropdown.Item href="/battleship">Battleship</NavDropdown.Item></Link>
+                            <NavDropdown.Item disabled>Hangman (soon)</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+                <Navbar.Collapse className="justify-content-end">
+                    {
+                        this.state.isSignedIn ?
+                            <Navbar.Text>
+                                Signed in as: <Link to="/profile">{this.state.name}</Link>
+                            </Navbar.Text> : <GoogleLogin
+                                clientId="601543072771-et5trbeu8n554ns5blk0j914274a412o.apps.googleusercontent.com"
+                                onSuccess={this.responseGoogle}
+                                onFailure={this.responseGoogle}
+                                isSignedIn={true}
+                                cookiePolicy={'single_host_origin'}
+                            />
+                    }
+                </Navbar.Collapse>
+            </Navbar>
+            <Container>
+
+                <Switch>
+                    <Route path="/battleship">
+                        <Battleship/>
+                    </Route>
+                    <Route path="/profile">
+                        <Profile/>
+                    </Route>
+                    <Route path="/">
+                        <Home/>
+                    </Route>
+                </Switch>
+
+            </Container>
+        </Router>
+    );
+}
+
+function Home() {
+    return <h2>Home</h2>;
+}
+
+function Profile() {
+    return <div>
+        <h2>Profile</h2>
+        <GoogleLogout
             clientId="601543072771-et5trbeu8n554ns5blk0j914274a412o.apps.googleusercontent.com"
-            onSuccess={responseGoogle}
-            onFailure={responseGoogle}
-            isSignedIn={true}
-            cookiePolicy={'single_host_origin'}
+            onLogoutSuccess={() => {console.log("logout")}}
         />
-        <div className="g-signin2" data-onsuccess="onSignIn"/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    </div>;
 }
 
-export default App;
+function Battleship() {
+    return <h2>Battlehsip</h2>;
+}
